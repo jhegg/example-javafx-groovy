920d15b15e5bb4ab6dd2bd2fc212eb7bf97dfb54
jhegg <josh.hegg@gmail.com>
2015-02-16 16:20:51 -0800

    Hooked up an about box

--- src/main/groovy/com/jhegg/github/notifier/RootLayoutController.groovy
+++ src/main/groovy/com/jhegg/github/notifier/RootLayoutController.groovy
@@ -1,10 +1,22 @@
 package com.jhegg.github.notifier
 
 import javafx.fxml.FXML
+import javafx.scene.control.Alert
+import javafx.stage.StageStyle
 
 class RootLayoutController {
     @FXML
-    void menuItemExit() {
+    void exit() {
         App.exitApp()
+    }
+
+    @FXML
+    void about() {
+        Alert aboutBox = new Alert(Alert.AlertType.INFORMATION)
+        aboutBox.setTitle("About")
+        aboutBox.setHeaderText(null)
+        aboutBox.setContentText("This example app illustrates using Groovy and JavaFX to interact with the GitHub API.")
+        aboutBox.initStyle(StageStyle.UTILITY)
+        aboutBox.showAndWait()
     }
 }
